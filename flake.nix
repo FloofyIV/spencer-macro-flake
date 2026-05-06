6961da32c5647d2e7a698a5c7671cb4843209619
Floofy <53851147+FloofyIV@users.noreply.github.com>
2026-05-07 09:25:32 +1200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -128,7 +128,7 @@ EOF
         };
 
         apps.default = flake-utils.lib.mkApp {
-          drv = self.packages.${system}.default;
+          drv = self.packages.${stdenv.hostPlatform.system}.default;
         };
       })
     // {
