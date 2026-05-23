0d95d838a0085326447bc483b03311c23af8f88c
Floofy <53851147+FloofyIV@users.noreply.github.com>
2026-05-24 04:28:23 +1200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -47,8 +47,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "Spencer0187";
             repo = "Spencer-Macro-Utilities";
-            rev = "c705ba92a385bd6834f34eb3dae0915fad9e6834";
-            sha256 = "sha256-moC0qUeL/i7Ta2mc70YOcMAvpFdgCRUujChxuHZ91Q8=";
+            rev = "linux-lagswitch-native";
+            sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
           };
 
           nativeBuildInputs = with pkgs; [
